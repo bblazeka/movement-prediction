a197e52f6d18521dd3290580095761df81e684aa
Bruno Blažeka <bblazeka@outlook.com>
2018-05-09 15:37:12 +0200

    work started

---
+++ Seminar/DataHandler.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv
+
+def loadCsv(filename):
+	lines = csv.reader(open(filename, "rb"))
+	dataset = list(lines)
+	for i in range(len(dataset)):
+		dataset[i] = [float(x) for x in dataset[i]]
+	return dataset
