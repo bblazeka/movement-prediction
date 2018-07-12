b56156c9062772a1b33fc2091687ee2c2697b0ed
Bruno <bblazeka@outlook.com>
2018-07-12 08:34:08 +0200

    added examples

---
+++ examples/mlp.py
@@ -0,0 +1,44 @@
+# http://scikit-learn.org/stable/auto_examples/neural_networks/plot_mnist_filters.html#
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_mldata
+from sklearn.neural_network import MLPClassifier
+
+from scipy.io import loadmat
+
+mnist_raw = loadmat("../data/examples/mnist-original.mat")
+mnist = {
+    "data": mnist_raw["data"].T,
+    "target": mnist_raw["label"][0],
+    "COL_NAMES": ["label", "data"],
+    "DESCR": "mldata.org dataset: mnist-original",
+}
+# rescale the data, use the traditional train/test split
+X, y = mnist["data"] / 255., mnist["target"]
+X_train, X_test = X[:60000], X[60000:]
+y_train, y_test = y[:60000], y[60000:]
+
+opt = 2
+
+if(opt == 1):
+    mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=400, alpha=1e-4,
+                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
+else:
+    mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
+                    solver='sgd', verbose=10, tol=1e-4, random_state=1,
+                    learning_rate_init=.1)
+
+mlp.fit(X_train, y_train)
+print("Training set score: %f" % mlp.score(X_train, y_train))
+print("Test set score: %f" % mlp.score(X_test, y_test))
+
+fig, axes = plt.subplots(4, 4)
+# use global min / max to ensure all weights are shown on the same scale
+vmin, vmax = mlp.coefs_[0].min(), mlp.coefs_[0].max()
+for coef, ax in zip(mlp.coefs_[0].T, axes.ravel()):
+    ax.matshow(coef.reshape(28, 28), cmap=plt.cm.gray, vmin=.5 * vmin,
+               vmax=.5 * vmax)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+plt.show()
