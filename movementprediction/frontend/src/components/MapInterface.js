786359f2dbd3a5a6170eee192b3d0788236610c0
Bruno <bblazeka@outlook.com>
2018-11-01 12:04:56 +0100

    moved to mapbox regression usability

--- movementprediction/frontend/src/components/MapInterface.js
+++ movementprediction/frontend/src/components/MapInterface.js
@@ -1,42 +1,55 @@
 import React from 'react';
-import Marker from './Marker.js';
-import GoogleMapReact from 'google-map-react'
+import mapboxgl from 'mapbox-gl';
+import { ELOOP } from 'constants';
 
-const googlemaps = require('../../../../apikeys.json')
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhbmNhIiwiYSI6ImNqbnliMmVoeTA1MTMzcG54Y3h2bnFtYmwifQ.o6CzepqOg00rpv7wKNeOuQ';
+
 export default class MapInterface extends React.Component {
     static defaultProps = {
         center: { lat: 41.15, lng: -8.61 },
         zoom: 12
       }
 
-    rendercomponents(data){
-        let marks = []
-        for(let ind in data){
-            let point = data[ind]
-            marks.push(
-                <Marker
-                    key={ point.id }
-                    lat={ point.lat }
-                    lng={ point.long }
-                />)
-        }
-        return marks
+    componentDidMount() {
+
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [this.props.center.lng, this.props.center.lat],
+            zoom: this.props.zoom
+        });
+
+        this.props.data.forEach(function(marker) {
+            // create a HTML element for each feature
+            var el = document.createElement('div');
+            el.className = 'marker';             
+            el.style.width = '5px';                                                      
+            el.style.height = '5px';
+            el.style.borderRadius = '50%';
+            el.style.border = '1px solid gray';
+            el.style.backgroundColor = 'lightblue';
+
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+            .setLngLat([marker.long,marker.lat])
+            .addTo(map);
+        });
     }
 
     render() {
-        console.log(this.props.data.length)
+        let lng = this.props.center.lng
+        let lat = this.props.center.lat
+        let zoom = this.props.zoom
         return (
             !this.props.data.length ? (
               <p>Nothing to show</p>
             ) : (
-                <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{key: googlemaps.googlemaps}}
-                  defaultCenter={ this.props.center }
-                  defaultZoom={ this.props.zoom }>
-                    {this.rendercomponents(this.props.data)}
-                </GoogleMapReact>
-              </div>
+                <div>
+                  <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+                    <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+                  </div>
+                  <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+                </div>
             )
         )
     }
--- movementprediction/points/views.py
+++ movementprediction/points/views.py
@@ -1,8 +1,8 @@
 from points.models import Point
 from points.serializers import PointSerializer
 from rest_framework import generics
+from . import Regression
 
 class PointListCreate(generics.ListCreateAPIView):
-    queryset = Point.objects.all()
-    print(len(queryset))
+    queryset = Regression.regress("41.15,-8.61,41.15,-8.61,41.15,-8.61","A")
     serializer_class = PointSerializer
